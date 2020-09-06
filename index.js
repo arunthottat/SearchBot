0c94be347e84a795bc120d56ba439a547b523204
arunthottat <70825759+arunthottat@users.noreply.github.com>
2020-09-06 09:36:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ mongoose.connect(process.env.DB_CONNECTION,
     console.log('connected to mLab db chatbot');
 }); */
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port,() => console.log(`Listening on port ${port}....`));
 
 function validateQuestion(question){
